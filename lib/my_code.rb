e4390ddaf8fcff8310f233de75483c3b27d8ba6c
Andrew Zajac <ajzajac@icloud.com>
2019-08-12 18:49:16 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
 # Your Code Here
+
+def map(array)
+  newarray = []
+  i = 0
+  while i < array.length do
+    newarray.push(yield(array[i]))
+    i += 1
+  end
+  newarray
+end
+
+def reduce(array, starting_point=0)
+  if starting_point == 0 
+    starting_point = array[0]
+    i = 1
+    while i < array.length do
+      starting_point = yield(starting_point, array[i])
+      i += 1
+    end
+    return starting_point
+  else
+    i = 0
+    while i < array.length do
+      starting_point = yield(starting_point, array[i])
+      i += 1
+    end
+    return starting_point
+  end
+end
