31c5c375774d7059a9802304a4688a9d425f0507
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-14 20:44:33 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+def map(array)
+  array.map { |x|*-1 }
+end
+
+def map(array)
+  array.map { |x| }
+end 
+
+def map(array)
+array.map { |x| x*2 }
+end
+
+def map(array)
+  array.map { |x| x*x }
+end
+
+def reduce(source_array)
+  source_array.reduce {|memo, x| memo + x}
+end
+
+def reduce(source_array, starting_point)
+  source_array.reduce {|starting_point, n| starting_point + n}
+end
+
+def reduce(source_array)
+  source_array.reduce {|memo, n| memo && n}
+end
+
