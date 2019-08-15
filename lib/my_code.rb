7a9d37e4b79b86e5c8d9cd5dad2d671b73bc1eeb
Thomas Castagna <castagna.tm@gmail.com>
2019-08-15 23:52:45 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,34 @@
-# Your Code Here
+def map (array, num = 1)
+	new_a =  []
+	i = 0
+	while i < array.length do
+		new_a.push(yield(array[i], num))
+		i += 1
+	end
+  return new_a
+end
+	map ([1,2,3]){|value, multiple| value *= multiple}
+
+  def reduce (array, starting_point = 0)
+    i = 0
+  	sum = starting_point
+    result = nil
+    while i < array.length do
+    	sum = yield(array[i], sum)
+			if array[i] != nil
+      	if array[i] == true
+        	result = true
+				elsif array[i] == false
+					result = false
+				end
+      end
+      i += 1
+    end
+	   if result == false || result == true
+       return result
+	    else
+		   return sum
+	    end
+  end
+
+  reduce([1,2,3]){ |x, n| x + n }
