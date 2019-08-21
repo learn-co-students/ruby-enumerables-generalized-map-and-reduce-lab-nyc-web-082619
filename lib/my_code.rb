ccb20d53559ac159361b25d7c8efd60258830bfc
Jiayi Wang <clairejw1015@gmail.com>
2019-08-21 20:45:41 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
 # Your Code Here
+def map(array)
+  new_array = [ ]
+  n = 0
+  
+  while n < array.length do
+    new_array.push << yield(array[n])
+    n += 1
+  end
+  
+  return new_array
+end
+
+def reduce(array, starting_point = nil)
+  
+  if starting_point 
+    n = 0
+    sum = starting_point
+  else
+    n = 1
+    sum = array[0]
+  end
+  
+  while n < array.length do 
+    sum = yield(sum, array[n])
+    n += 1
+  end
+  
+  return sum
+end
